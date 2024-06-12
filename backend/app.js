527a17a36cfa79d917dcef6bf8d2b530b7fb05e7
Shyam jee <93826494+shyamjee1645@users.noreply.github.com>
2024-06-12 12:14:24 +0000

    Final Connection of Frontend and Backend

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const peopleRouter = require('./routes/peopleRoute');
-const homeRouter = require('./routes/homeRoute');
+const peopleRouter = require("./routes/peopleRoute");
+const homeRouter = require("./routes/homeRoute");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const testapiRouter = require('./routes/testApi');
-
+const testapiRouter = require("./routes/testApi");
+var cors = require("cors");
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "backend/config/config.env" });
-  }
-  
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(fileUpload());
-  
-app.use('/', homeRouter);
-app.use('/api/people', peopleRouter);
-app.use('/testapi', testapiRouter);
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-module.exports = app
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(cors());
+
+app.use("/", homeRouter);
+app.use("/api/people", peopleRouter);
+app.use("/testapi", testapiRouter);
+
+module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import NavbarHome from './components/navbar/navbar';
-import Footer from './components/layout/footer/footer';
+import React, { Component } from "react";
+import NavbarHome from "./components/navbar/navbar";
+import Footer from "./components/layout/footer/footer";
+import { Container } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <NavbarHome />
-      <div style={{ flex: 1,backgroundColor: '#ffe6d0',}}>
-        {/* Content goes here */}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("https://yntrwx-4000.csb.app/home")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    // Use componentDidMount instead of componentWillMount
+    this.callAPI();
+  }
+
+  render() {
+    // Added missing parenthesis here
+    return (
+      <div
+        className="App"
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <NavbarHome />
+        <div style={{ flex: 1, backgroundColor: "#ffe6d0" }}>
+          <p className="App-intro">;{this.state.apiResponse}</p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
